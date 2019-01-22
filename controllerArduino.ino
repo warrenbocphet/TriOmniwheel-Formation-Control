6ed52e486bd5c0ecdfe9da37000f6dfe1177b087
vitsensei <42131264+vitsensei@users.noreply.github.com>
2019-01-22 11:49:08 +1030

    Update controllerArduino.ino
    Added HPF to Gyro readings, LPF to Accel readings and complimentary filter for both afterwards.

--- controllerArduino.ino
+++ controllerArduino.ino
@@ -4,16 +4,43 @@
 // The SFE_LSM9DS1 library requires both Wire and SPI be
 // included BEFORE including the 9DS1 library.
 #include <Wire.h>
-#include <SPI.h>
 #include <SparkFunLSM9DS1.h>
-#include <SimpleKalmanFilter.h>
 
 //Stepper X, Y, Z are the same as the X, Y, Z on Protoneer shield
 AccelStepper stepper0(AccelStepper::DRIVER, 2, 5); // pin 2 for step, pin 5 for direction
 AccelStepper stepper1(AccelStepper::DRIVER, 3, 6);
 AccelStepper stepper2(AccelStepper::DRIVER, 4, 7);
 
-// For serial communication
+//////////////////////// For filtering ///////////////////////////
+
+// gyroscope
+float gyro0_previous, gyro1_previous, gyro2_previous;
+float gyro0_now, gyro1_now, gyro2_now;
+float HPF_gyro0 = 0, HPF_gyro1 = 0, HPF_gyro2 = 0;
+
+// accelerometer
+float rollAngle_previous0 = 0, rollAngle_now0 = 0;
+float rollAngle_previous1 = 0, rollAngle_now1 = 0;
+float rollAngle_previous2 = 0, rollAngle_now2 = 0;
+
+float deltaAngle0 = 0, deltaAngle1 = 0, deltaAngle2 = 0;
+float LPF_accel0 = 0, LPF_accel1 = 0, LPF_accel2 = 0;
+
+// filtered variable
+float filteredX, filteredY, filteredZ;
+int filter_flag = 0;
+
+// high pass and low pass filter
+const float alpha = 0.7;
+
+// complimentary filter
+const float gyroCoef = 0.6;
+const float accCoef = 0.4;
+
+float comWheel0 = 0, comWheel1 = 0, comWheel2 = 0;
+
+
+////////////////////////////////// For serial communication //////////////////////////////////
 byte a;
 byte buf[9];
 byte targetX_H;
@@ -102,30 +129,33 @@ int threshold_velocity = 400;
 
 float time_limit = 0.0;
 
-// For IMU sensor
+//////////////////////////// For IMU sensor ////////////////////////
 // objects of classes
-LSM9DS1 imu_X;
-LSM9DS1 imu_Y;
-LSM9DS1 imu_Z;
-SimpleKalmanFilter KF_X(1, 1, 1);
-SimpleKalmanFilter KF_Y(1, 1, 1);
-SimpleKalmanFilter KF_Z(1, 1, 1);
+LSM9DS1 imu0;
+LSM9DS1 imu1;
+LSM9DS1 imu2;
 
 #define LSM9DS1_AG  0x6B // accelerometer and gyrometer
 #define LSM9DS1_M  0x1E // magnetometer
 #define TCAADDR 0x70 // multiplexer i2c
 
-float offset_wheel_X, offset_wheel_Y, offset_wheel_Z;
-float threshold_X, threshold_Y, threshold_Z;
+// gyroscope
+float gyroOffset0, gyroOffset1, gyroOffset2;
+float gyro0, gyro1, gyro2;
 
-float gyroX, gyroY, gyroZ;
-float linear_X, linear_Y, linear_Z;
-float fusion_X, fusion_Y, fusion_Z;
-float filter_X, filter_Y, filter_Z;
-float filter_X_previous, filter_Y_previous, filter_Z_previous;
-float filter_X_now, filter_Y_now, filter_Z_now;
-float filter_X_avr, filter_Y_avr, filter_Z_avr;
-int filter_flag = 0;
+// accelerometer
+float accelX0 = 0, accelY0 = 0, accelZ0 = 0;
+float accelX1 = 0, accelY1 = 0, accelZ1 = 0;
+float accelX2 = 0, accelY2 = 0, accelZ2 = 0;
+float accelOffsetX0 = 0, accelOffsetY0 = 0, accelOffsetZ0 = 0;
+float accelOffsetX1 = 0, accelOffsetY1 = 0, accelOffsetZ1 = 0;
+float accelOffsetX2 = 0, accelOffsetY2 = 0, accelOffsetZ2 = 0;
+
+struct accelOffset {
+	float x;
+	float y;
+	float z;
+};
 
 float DegreesToRadians(float degrees)
 {
@@ -158,25 +188,35 @@ float calibrateGyro(LSM9DS1 imu)
   
 }
 
-// find variance of 100 reading samples
-float thresholdGyro(LSM9DS1 imu, float mean)
+struct accelOffset calibrateAccel(LSM9DS1 imu)
 {
-  float tempValue;
-  float threshold = 0;
+  float valueX = 0;
+  float valueY = 0;
+  float valueZ = 0;
+
+  float offsetX = 0;
+  float offsetY = 0;
+  float offsetZ = 0;
+
   float limit = 100;
-  
-  // find threshold value
+
+  struct accelOffset offset;
+
+  // find mean value 
   for(int i = 0; i < limit; i++)
   {
-    imu.readGyro();
-    tempValue = imu.gz - mean;
-    if (abs(tempValue) > abs(threshold)){
-      threshold = tempValue;
-    }
+    imu.readAccel();
+    valueX += imu.ax;
+    valueY += imu.ay;
+    valueZ += imu.az;
   }
-  return abs(threshold)/2.0;
-}
 
+  offset.x = valueX/limit;
+  offset.y = valueY/limit;
+  offset.z = valueZ/limit;
+
+  return offset;
+}
 
 // for serial communication
 int twobytes1int(byte high, byte low)
@@ -231,38 +271,56 @@ void setup()
   stepper2.setSpeed(0);
 
   // for IMU sensor
-  imu_X.settings.device.commInterface = IMU_MODE_I2C;
-  imu_X.settings.device.mAddress = LSM9DS1_M;
-  imu_X.settings.device.agAddress = LSM9DS1_AG;
+  imu0.settings.device.commInterface = IMU_MODE_I2C;
+  imu0.settings.device.mAddress = LSM9DS1_M;
+  imu0.settings.device.agAddress = LSM9DS1_AG;
   
-  imu_Y.settings.device.commInterface = IMU_MODE_I2C;
-  imu_Y.settings.device.mAddress = LSM9DS1_M;
-  imu_Y.settings.device.agAddress = LSM9DS1_AG;
+  imu1.settings.device.commInterface = IMU_MODE_I2C;
+  imu1.settings.device.mAddress = LSM9DS1_M;
+  imu1.settings.device.agAddress = LSM9DS1_AG;
   
-  imu_Z.settings.device.commInterface = IMU_MODE_I2C;
-  imu_Z.settings.device.mAddress = LSM9DS1_M;
-  imu_Z.settings.device.agAddress = LSM9DS1_AG;
+  imu2.settings.device.commInterface = IMU_MODE_I2C;
+  imu2.settings.device.mAddress = LSM9DS1_M;
+  imu2.settings.device.agAddress = LSM9DS1_AG;
 
   /* Initialise the 1st sensor */
   tcaselect(0);
-  while(!imu_X.begin());  
-  offset_wheel_X = calibrateGyro(imu_X);
-  // threshold_X = thresholdGyro(imu_X, offset_wheel_X);
-  // threshold_X = imu_X.calcGyro(threshold_X);
+  while(!imu0.begin());  
+  gyroOffset0 = calibrateGyro(imu0);
+  struct accelOffset offset0;
+  offset0 = calibrateAccel(imu0);
+  accelOffsetX0 = offset0.x;
+  accelOffsetY0 = offset0.y;
+  accelOffsetZ0 = offset0.z;
+
+  // threshold_X = thresholdGyro(imu0, gyroOffset0);
+  // threshold_X = imu0.calcGyro(threshold_X);
   
   /* Initialise the 2nd sensor */
   tcaselect(1);
-  while(!imu_Y.begin());
-  offset_wheel_Y = calibrateGyro(imu_Y);
-  // threshold_Y = thresholdGyro(imu_Y, offset_wheel_Y);
-  // threshold_Y = imu_Y.calcGyro(threshold_Y);
+  while(!imu1.begin());
+  gyroOffset1 = calibrateGyro(imu1);
+  struct accelOffset offset1;
+  offset1 = calibrateAccel(imu1);
+  accelOffsetX1 = offset1.x;
+  accelOffsetY1 = offset1.y;
+  accelOffsetZ1 = offset1.z;
+
+  // threshold_Y = thresholdGyro(imu1, gyroOffset1);
+  // threshold_Y = imu1.calcGyro(threshold_Y);
   
    /* Initialise the 3rd sensor */
   tcaselect(2);
-  while(!imu_Z.begin());
-  offset_wheel_Z = calibrateGyro(imu_Z);
-  // threshold_Z = thresholdGyro(imu_Z, offset_wheel_Z);
-  // threshold_Z = imu_Z.calcGyro(threshold_Z);
+  while(!imu2.begin());
+  gyroOffset2 = calibrateGyro(imu2);
+  struct accelOffset offset2;
+  offset2 = calibrateAccel(imu2);
+  accelOffsetX2 = offset2.x;
+  accelOffsetY2 = offset2.y;
+  accelOffsetZ2 = offset2.z;
+
+  // threshold_Z = thresholdGyro(imu2, gyroOffset2);
+  // threshold_Z = imu2.calcGyro(threshold_Z);
 
   start1 = millis();
 }
@@ -395,9 +453,9 @@ void loop()
        Serial.write(3);
      }
 
-     if (filter_X_avr < 0) 
+     if (filteredX < 0) 
       {
-        v0_send = (int) 100*filter_X_avr*(-1);
+        v0_send = (int) 100*filteredX*(-1);
         v0_send_h = ((v0_send >> 8) & 0xFF);
         v0_send_l = (v0_send & 0xFF);  
         Serial.write(v0_send_h);
@@ -406,7 +464,7 @@ void loop()
       }
       else
       {
-       v0_send = (int) 100*filter_X_avr;
+       v0_send = (int) 100*filteredX;
        v0_send_h = ((v0_send >> 8) & 0xFF);
        v0_send_l = (v0_send & 0xFF); 
        Serial.write(v0_send_h);
@@ -414,9 +472,9 @@ void loop()
        Serial.write(3);
      }
 
-     if (filter_Y_avr < 0) 
+     if (filteredY < 0) 
       {
-        v1_send = (int) 100*filter_Y_avr*(-1);
+        v1_send = (int) 100*filteredY*(-1);
         v1_send_h = ((v1_send >> 8) & 0xFF);
         v1_send_l = (v1_send & 0xFF);  
         Serial.write(v1_send_h);
@@ -425,7 +483,7 @@ void loop()
       }
       else
       {
-       v1_send = (int) 100*filter_Y_avr;
+       v1_send = (int) 100*filteredY;
        v1_send_h = ((v1_send >> 8) & 0xFF);
        v1_send_l = (v1_send & 0xFF); 
        Serial.write(v1_send_h);
@@ -433,9 +491,9 @@ void loop()
        Serial.write(3);
      }
 
-     if (filter_Z_avr < 0) 
+     if (filteredZ < 0) 
       {
-        v2_send = (int) 100*filter_Z_avr*(-1);
+        v2_send = (int) 100*filteredZ*(-1);
         v2_send_h = ((v2_send >> 8) & 0xFF);
         v2_send_l = (v2_send & 0xFF);  
         Serial.write(v2_send_h);
@@ -444,7 +502,7 @@ void loop()
       }
       else
       {
-       v2_send = (int) 100*filter_Z_avr;
+       v2_send = (int) 100*filteredZ;
        v2_send_h = ((v2_send >> 8) & 0xFF);
        v2_send_l = (v2_send & 0xFF); 
        Serial.write(v2_send_h);
@@ -508,66 +566,99 @@ void loop()
   stepper2.setSpeed(v2s);
 
   // get IMU reading
-  // wheel X
+  // wheel 0
     tcaselect(0);
-    imu_X.readGyro();
-    gyroX = imu_X.calcGyro(imu_X.gz - offset_wheel_X);
-    // if (abs(gyroX) < threshold_X)
-    //   gyroX = 0;
-    linear_X = DegreesToRadians(gyroX) * wheel_radius; // deg/s to rad/s
-    fusion_X = linear_X; // will add step count later
-    filter_X = KF_X.updateEstimate(fusion_X);
+    if (imu0.gyroAvailable())
+    	imu0.readGyro();
+    gyro0 = imu0.calcGyro(imu0.gz - gyroOffset0);
+
+    if (imu0.accelAvailable())
+    	imu0.readAccel();
+
+    accelX0 = imu0.calcAccel(imu0.ax - accelOffsetX0);
+    accelY0 = imu0.calcAccel(imu0.ay - accelOffsetY0);
+    accelZ0 = imu0.calcAccel(imu0.az - accelOffsetZ0);
+
+    // linear_X = DegreesToRadians(gyro0) * wheel_radius; // deg/s to rad/s
+
     if (filter_flag == 1)
     {
-      filter_X_now = filter_X;
-      filter_X_avr = (filter_X_now + filter_X_previous)/2.0;
-    } else {
-      filter_X_avr = 0;
-    }
-    filter_X_previous = filter_X;
+      gyro0_now = gyro0;
+      HPF_gyro0 = HPF_gyro0*alpha + alpha*(gyro0_now - gyro0_previous);
 
-  // wheel Y
+      rollAngle_now0 = atan2(accelX0, accelY0);
+      deltaAngle0 = rollAngle_now0 - rollAngle_previous0;
+      LPF_accel0 = (deltaAngle0/elapsedTime)*alpha + (LPF_accel0*(1.0 - alpha)); // in degree/s
+      comWheel0 = (gyroCoef*(comWheel0 + HPF_gyro0) + (accCoef*LPF_accel0));
+    } 
+
+    gyro0_previous = gyro0;
+    rollAngle_previous0 = atan2(accelX0, accelY0);
+
+  // wheel 1
     tcaselect(1);
-    imu_Y.readGyro();
-    gyroY = imu_Y.calcGyro(imu_Y.gz - offset_wheel_Y);
-    // if (abs(gyroY) < threshold_Y)
-    //   gyroY = 0;
-    linear_Y = DegreesToRadians(gyroY) * wheel_radius;
-    fusion_Y = linear_Y; // will add step count later
-    filter_Y = KF_Y.updateEstimate(fusion_Y);
-    if (filter_flag == 1)
-    {
-      filter_Y_now = filter_Y;
-      filter_Y_avr = (filter_Y_now + filter_Y_previous)/2.0;
-    } else {
-      filter_Y_avr = 0;
-    }
-    filter_Y_previous = filter_Y;
+    if (imu1.gyroAvailable())
+    	imu1.readGyro();
+    gyro1 = imu1.calcGyro(imu1.gz - gyroOffset1);
 
-  // wheel Z
-    tcaselect(2);
-    imu_Z.readGyro();
-    gyroZ = imu_Z.calcGyro(imu_Z.gz - offset_wheel_Z);
-    // if (abs(gyroZ) < threshold_Z)
-    //   gyroZ = 0;
-    linear_Z = DegreesToRadians(gyroZ) * wheel_radius;
-    fusion_Z = linear_Z; // will add step count later
-    filter_Z = KF_Z.updateEstimate(fusion_Z);
+    if (imu1.accelAvailable())
+    	imu1.readAccel();
+
+    accelX1 = imu1.calcAccel(imu1.ax - accelOffsetX1);
+    accelY1 = imu1.calcAccel(imu1.ay - accelOffsetY1);
+    accelZ1 = imu1.calcAccel(imu1.az - accelOffsetZ1);
+
+    // linear_Y = DegreesToRadians(gyro1) * wheel_radius;
+
     if (filter_flag == 1)
     {
-      filter_Z_now = filter_Z;
-      filter_Z_avr = (filter_Z_now + filter_Z_previous)/2.0;
-    } else {
-      filter_Z_avr = 0;
+      gyro1_now = gyro1;
+      HPF_gyro1 = HPF_gyro1*alpha + alpha*(gyro1_now - gyro1_previous);
+
+      rollAngle_now1 = atan2(accelX1, accelY1);
+      deltaAngle1 = rollAngle_now1 - rollAngle_previous1;
+      LPF_accel1 = (deltaAngle0/elapsedTime)*alpha + (LPF_accel1*(1.0 - alpha)); // in degree/s
+      comWheel1 = (gyroCoef*(comWheel1 + HPF_gyro1) + (accCoef*LPF_accel1));
     }
-    filter_Z_previous = filter_Z;
+
+    gyro1_previous = gyro1;
+    rollAngle_previous1 = atan2(accelX1, accelY1);
+
+  // wheel 2
+    tcaselect(2);
+    if (imu2.gyroAvailable())
+    	imu2.readGyro();
+    gyro2 = imu2.calcGyro(imu2.gz - gyroOffset2);
+
+    if (imu2.accelAvailable())
+    	imu2.readAccel();
+
+    accelX2 = imu2.calcAccel(imu2.ax - accelOffsetX2);
+    accelY2 = imu2.calcAccel(imu2.ay - accelOffsetY2);
+    accelZ2 = imu2.calcAccel(imu2.az - accelOffsetZ2);
+
+    // linear_Z = DegreesToRadians(gyro2) * wheel_radius;
+
+    if (filter_flag == 1)
+    {
+      gyro2_now = gyro2;
+      HPF_gyro2 = HPF_gyro2*alpha + alpha*(gyro2_now - gyro2_previous);
+
+      rollAngle_now2 = atan2(accelX2, accelY2);
+      deltaAngle2 = rollAngle_now2 - rollAngle_previous2;
+      LPF_accel2 = (deltaAngle0/elapsedTime)*alpha + (LPF_accel2*(1.0 - alpha)); // in degree/s
+      comWheel2 = (gyroCoef*(comWheel2 + HPF_gyro2) + (accCoef*LPF_accel2));
+    }
+
+    gyro2_previous = gyro2;
+    rollAngle_previous2 = atan2(accelX2, accelY2);
 
     filter_flag = 1;
 
   // recalculate v_x, v_y, w
-  w = 1/(3*rover_radius)*(filter_X_avr + filter_Y_avr + filter_Z_avr);
-  v = (sqrt(3)/3)*(filter_Z_avr - filter_X_avr);
-  v_n = 1/3.0*(filter_Z_avr + filter_X_avr) - 2/3.0*filter_Y_avr;
+  w = 1/(3*rover_radius)*(filteredX + filteredY + filteredZ);
+  v = (sqrt(3)/3)*(filteredZ - filteredX);
+  v_n = 1/3.0*(filteredZ + filteredX) - 2/3.0*filteredY;
 
   v_x = v*cos(phi) - v_n*sin(phi);
   v_y = v*sin(phi) + v_n*cos(phi);
