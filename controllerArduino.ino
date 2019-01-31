ad038c69a4de902a42b50ef19cd70f992138cb92
vitsensei <42131264+vitsensei@users.noreply.github.com>
2019-01-31 15:18:26 +1030

    Update controllerArduino.ino
    Use IMU sensors feedback.

--- controllerArduino.ino
+++ controllerArduino.ino
@@ -12,23 +12,117 @@ AccelStepper stepper1(AccelStepper::DRIVER, 3, 6);
 AccelStepper stepper2(AccelStepper::DRIVER, 4, 7);
 
 int resolution = 8;
-int threshold_velocity = 400;
+int threshold_velocity = 800; // in step/s
 float rover_radius = 19.3; // in cm
 float wheel_radius = 5.22; // in cm
-float constant_linearVelocity = 400*((wheel_radius*PI)/(resolution*100)); // in cm/s
+float constant_linearVelocity = 2*threshold_velocity*((wheel_radius*PI)/(resolution*100)); // in cm/s
 
-int i=0;
-int j=0;
-int m=0;
-int n=0;
+//////////////////// For serial communication ////////////////////
+byte a;
+byte buf[9];
+byte targetX_H;
+byte targetX_L;
+byte targetX_dir; // direction should be either 1 for CCW or 3 for CW. This number will later be...
+            // ...subtracted to 2, which then create -1 and 1. Remember, byte is an unsigned number.
+byte targetY_H; 
+byte targetY_L;
+byte targetY_dir;
 
-int a=-1;
+byte targetPhi_H;
+byte targetPhi_L;
+byte targetPhi_dir;
 
-int speed0 = -1;
-int speed1 = -1;
-int speed2 = -1;
+byte targetTime_H;
+byte targetTime_L;
+
+byte x_send_h;
+byte x_send_l;
+byte y_send_h;
+byte y_send_l;
+byte phi_send_h;
+byte phi_send_l;
+
+int x_send;
+int y_send;
+int phi_send;
+
+int v0_send;
+int v1_send;
+int v2_send;
+
+byte v0_send_h;
+byte v0_send_l;
+byte v1_send_h;
+byte v1_send_l;
+byte v2_send_h;
+byte v2_send_l;
+
+byte timer_send_h;
+byte timer_send_l;
+
+int start_flag = 0;
+
+int twobytes1int(byte high, byte low)
+{
+  int number;
+
+  number = low | (high << 8);
+  return number;
+}
+
 
 //////////////////////// For integration /////////////////////////
+
+float v0 = 0; // in cm/s
+float v1 = 0;
+float v2 = 0;
+
+float v0s = 0; // in step/s
+float v1s = 0;
+float v2s = 0;
+float largest_vel;
+
+float v = 0;
+float v_n = 0;
+float w = 0;
+
+float v_x = 0;
+float v_y = 0;
+
+float x = 0;
+float y = 0;
+float phi = 0;
+
+float targetX = 0;
+float targetY = 0;
+float targetPhi = 0;
+int targetTime = 0;
+
+float dx = 0;
+float dy = 0;
+float dphi = 0;
+
+float speed0 = 0, speed1 = 0, speed2 = 0;
+int speed0s = 0, speed1s = 0, speed2s = 0;
+
+float time_limit = 0.0;
+unsigned long timer = 0;
+
+float largest_of_three(float first, float second, float third)
+{
+  float largest = first;
+  if (abs(second) > abs(largest))
+  {
+    largest = second;
+  }
+
+  if (abs(third) > abs(largest))
+  {
+    largest = third;
+  }
+
+  return abs(largest);
+}
 
 
 //////////////////////////// For IMU sensor ////////////////////////
@@ -59,6 +153,18 @@ void tcaselect(uint8_t i) {
   Wire.endTransmission();  
 }
 
+void setupDevice(LSM9DS1 imu)
+{
+  // [commInterface] determines whether we'll use I2C or SPI
+  // to communicate with the LSM9DS1.
+  // Use either IMU_MODE_I2C or IMU_MODE_SPI
+  imu.settings.device.commInterface = IMU_MODE_I2C;
+
+  // [agAddress] sets the I2C address or SPI CS pin of the
+  // LSM9DS1's accelerometer/gyroscope.
+  imu.settings.device.agAddress = 0x6B; // I2C address 0x6B
+}
+
 void setupGyro(LSM9DS1 imu)
 {
   // [enabled] turns the gyro on or off.
@@ -84,6 +190,8 @@ void setupGyro(LSM9DS1 imu)
   // Allowable values are 0-9. Value depends on ODR.
   // (Datasheet section 7.14)
   imu.settings.gyro.HPFCutoff = 1; // HPF cutoff = 4Hz
+  imu.enableFIFO();
+  imu.setFIFO(FIFO_CONT, 0x01);
   // [flipX], [flipY], and [flipZ] are booleans that can
   // automatically switch the positive/negative orientation
   // of the three gyro axes.
@@ -116,11 +224,11 @@ float calibrateGyro(LSM9DS1 imu)
 ////////////////// For keeping track of time ////////////////
 unsigned long start1 = 0;
 unsigned int elapsedTime1 = 0;
+unsigned long start2 = 0;
+unsigned int elapsedTime2 = 0;
+
 unsigned int time_frame = 25;
 
-int start_flag = 0;
-int step_count = 0;
-unsigned long start2 = 0;
 
 ////////////////// For Filtering ////////////////////////////
 int filter_flag = 0;
@@ -191,30 +299,36 @@ void setup()
   pinMode(8, OUTPUT);
   digitalWrite(8, LOW);
 
-  stepper0.setMaxSpeed(8000);
-  stepper1.setMaxSpeed(8000);
-  stepper2.setMaxSpeed(8000);
+  stepper0.setMaxSpeed(800);
+  stepper1.setMaxSpeed(800);
+  stepper2.setMaxSpeed(800);
+
+  delay(1000);
 
   // for IMU sensor
   /* Initialise the 1st sensor */
   tcaselect(0);
+  setupDevice(imu0);
   while(!imu0.begin());  
   setupGyro(imu0);
   gyroOffset0 = calibrateGyro(imu0);
   
   /* Initialise the 2nd sensor */
   tcaselect(1);
+  setupDevice(imu1);
   while(!imu1.begin());
   setupGyro(imu1);
   gyroOffset1 = calibrateGyro(imu1);
   
    /* Initialise the 3rd sensor */
   tcaselect(2);
+  setupDevice(imu2);
   while(!imu2.begin());
   setupGyro(imu2);
   gyroOffset2 = calibrateGyro(imu2);
 
   start1 = millis();
+  start2 = millis();
 }
 
 void loop()
@@ -224,29 +338,239 @@ void loop()
    if (Serial.available())
    {
     a = Serial.read();
-    if (a == 0)
+  	if (a == 255) // get coordinate
     {
-      i++;
-      j = (i%8)*100;
-      m = -(i%8)*100;
-      n = pow(-1,i)*(i%8)*50;
-      
-      stepper0.setSpeed(j);
-      stepper1.setSpeed(m);
-      stepper2.setSpeed(n);
+      Serial.readBytes(buf, 12);
+      targetX_H = buf[0];
+      targetX_L = buf[1];
+      targetX_dir = buf[2];
 
-    } else if (a == 1) {    
-      Serial.println(speed0);
-      Serial.println(speed1);
-      Serial.println(speed2);
+      targetY_H = buf[3];
+      targetY_L = buf[4];
+      targetY_dir = buf[5];
+
+      targetPhi_H = buf[6];
+      targetPhi_L = buf[7];
+      targetPhi_dir = buf[8];
+
+      targetTime_H = buf[9];
+      targetTime_L = buf[10];
+
+      targetX = twobytes1int(targetX_H, targetX_L);
+      targetY = twobytes1int(targetY_H, targetY_L);
+      targetPhi= twobytes1int(targetPhi_H, targetPhi_L);
+      targetTime = twobytes1int(targetTime_H, targetTime_L);
+
+      targetX = targetX * (targetX_dir - 2);
+      targetY = targetY * (targetY_dir - 2);
+      targetPhi = targetPhi * (targetPhi_dir - 2);
+      targetPhi = targetPhi/180.0*PI;
+
+      start_flag = 1;
+      start1 = millis();
+      start2 = millis();
+
+    }
+
+    if (a == 254) // reset
+    {
+      stepper0.stop();
+      stepper1.stop();
+      stepper2.stop();
+
+      x = 0;
+      y = 0;
+      phi = 0;
+      start1 = millis();
+      timer = 0;
+    }
+
+    if (a == 253) // send state to RPI
+    {
+      Serial.write(255);
+      if (x < 0) 
+      {
+        x_send = (int) x*(-1);
+        x_send_h = ((x_send >> 8) & 0xFF);
+        x_send_l = (x_send & 0xFF);  
+        Serial.write(x_send_h);
+        Serial.write(x_send_l);
+        Serial.write(1);
+      }
+      else
+      {
+       x_send = (int) x;
+       x_send_h = ((x_send >> 8) & 0xFF);
+       x_send_l = (x_send & 0xFF); 
+       Serial.write(x_send_h);
+       Serial.write(x_send_l);
+       Serial.write(3);
+     }
+    
+     if (y < 0) 
+     {
+       y_send = (int) y*(-1);
+       y_send_h = ((y_send >> 8) & 0xFF);
+       y_send_l = (y_send & 0xFF);
+       Serial.write(y_send_h);
+       Serial.write(y_send_l);  
+       Serial.write(1);    
+     }
+     else
+     {
+       y_send = (int) y;
+       y_send_h = ((y_send >> 8) & 0xFF);
+       y_send_l = (y_send & 0xFF); 
+       Serial.write(y_send_h);
+       Serial.write(y_send_l);
+       Serial.write(3);
+     }
+    
+     if (phi < 0) 
+     {
+       phi_send = (int) (phi*(180/3.14)*(-1));
+       phi_send_h = ((phi_send >> 8) & 0xFF);
+       phi_send_l = (phi_send & 0xFF);  
+       Serial.write(phi_send_h);
+       Serial.write(phi_send_l);
+       Serial.write(1);
+     }
+     else
+     {
+       phi_send = (int) (phi*(180/3.14));
+       phi_send_h = ((phi_send >> 8) & 0xFF);
+       phi_send_l = (phi_send & 0xFF); 
+       Serial.write(phi_send_h);
+       Serial.write(phi_send_l);
+       Serial.write(3);
+     }
+
+     if (v0 < 0) 
+      {
+        v0_send = (int) 100*speed0s*(-1);
+        v0_send_h = ((v0_send >> 8) & 0xFF);
+        v0_send_l = (v0_send & 0xFF);  
+        Serial.write(v0_send_h);
+        Serial.write(v0_send_l);
+        Serial.write(1);
+      }
+      else
+      {
+       v0_send = (int) 100*speed0s;
+       v0_send_h = ((v0_send >> 8) & 0xFF);
+       v0_send_l = (v0_send & 0xFF); 
+       Serial.write(v0_send_h);
+       Serial.write(v0_send_l);
+       Serial.write(3);
+     }
+
+     if (v1 < 0) 
+      {
+        v1_send = (int) 100*speed1s*(-1);
+        v1_send_h = ((v1_send >> 8) & 0xFF);
+        v1_send_l = (v1_send & 0xFF);  
+        Serial.write(v1_send_h);
+        Serial.write(v1_send_l);
+        Serial.write(1);
+      }
+      else
+      {
+       v1_send = (int) 100*speed1s;
+       v1_send_h = ((v1_send >> 8) & 0xFF);
+       v1_send_l = (v1_send & 0xFF); 
+       Serial.write(v1_send_h);
+       Serial.write(v1_send_l);
+       Serial.write(3);
+     }
+
+     if (v2 < 0) 
+      {
+        v2_send = (int) 100*speed2s*(-1);
+        v2_send_h = ((v2_send >> 8) & 0xFF);
+        v2_send_l = (v2_send & 0xFF);  
+        Serial.write(v2_send_h);
+        Serial.write(v2_send_l);
+        Serial.write(1);
+      }
+      else
+      {
+       v2_send = (int) 100*speed2s;
+       v2_send_h = ((v2_send >> 8) & 0xFF);
+       v2_send_l = (v2_send & 0xFF); 
+       Serial.write(v2_send_h);
+       Serial.write(v2_send_l);
+       Serial.write(3);
+
+      }
+
+      timer_send_h = (timer >> 8) & 0xFF;
+      timer_send_l = (timer & 0xFF);
+      Serial.write(timer_send_h);
+      Serial.write(timer_send_l);
     }
     
    }
 
-   if (elapsedTime1 > time_frame) // get IMU reading
+   if (elapsedTime1 > time_frame && start_flag == 1)
    {
       start1 = millis();
+
+      stepper0.runSpeed();
+      stepper1.runSpeed();
+      stepper2.runSpeed();
+
+      ///////////// Controller (calculate linear velocity) ////////////
+
+      // find dx, dy, dphi
+	  dx = targetX - x;
+	  dy = targetY - y;
+	  dphi = targetPhi - phi;
+	  
+	  time_limit = sqrt(dx*dx + dy*dy);
+
+	  if (time_limit == 0)
+	  {
+	   time_limit = 1;
+	  }
+	  
+	  v_x = dx/time_limit;
+	  v_y = dy/time_limit;
+	  w = dphi/time_limit;
+
+	  /////////// Calculate require speed of 3 wheels //////////////
+
+	  // find v, v_n
+	  v = v_x*cos(phi) + v_y*sin(phi);
+	  v_n = -v_x*sin(phi) + v_y*cos(phi);
+
+	  // find v_0, v_1, v_2
+	  v0 = -v*sin(PI/3) + v_n*cos(PI/3) + w*rover_radius;
+	  v1 =              - v_n           + w*rover_radius;
+	  v2 =  v*sin(PI/3) + v_n*cos(PI/3) + w*rover_radius;
+
+	  // convert it back to step/s
+   	  v0s = v0 * ((resolution*100)/(wheel_radius*PI));
+   	  v1s = v1 * ((resolution*100)/(wheel_radius*PI));
+   	  v2s = v2 * ((resolution*100)/(wheel_radius*PI));
+
+   	  // scale down v_0, v_1, v_2 if necessary
+	  if (abs(v0s) > threshold_velocity || abs(v1s) > threshold_velocity || abs(v2s) > threshold_velocity)
+	  {
+	    // find the largest velocity
+	    largest_vel = largest_of_three(v0s, v1s, v2s);
+	  
+	    // scale down
+	    v0s = v0s/largest_vel*threshold_velocity;
+	    v1s = v1s/largest_vel*threshold_velocity;
+	    v2s = v2s/largest_vel*threshold_velocity;
+	  }
+
+      stepper0.runSpeed();
+      stepper1.runSpeed();
+      stepper2.runSpeed();
       
+
+      ///////////// Getting data and filtering data /////////////
       ///////////// wheel 0 ///////////////
       tcaselect(0);
       if (imu0.gyroAvailable())
@@ -256,11 +580,16 @@ void loop()
       if (filter_flag == 1)
       {
         // kalman filter
-      	filtered0 = KF_filter(0, gyroReading0);
+      	filtered0 = KF_filter(0, gyroReading0); // degree/s
 
         // converting unit
-        speed0 = (filtered0/360.0)*1600.0; // from degree/s to step/s
+        speed0 = ((filtered0/360.0)*(2*PI))*wheel_radius; // convert to rad/s then cm/s
+        speed0s = (filtered0/360.0)*1600;
       } 
+
+      stepper0.runSpeed();
+      stepper1.runSpeed();
+      stepper2.runSpeed();
 
       ///////////// wheel 1 ///////////////
       tcaselect(1);
@@ -271,11 +600,16 @@ void loop()
       if (filter_flag == 1)
       {
         // kalman filter
-        filtered1 = KF_filter(1, gyroReading1);
+        filtered1 = KF_filter(1, gyroReading1); // degree/s
       
         // converting unit
-        speed1 = (filtered1/360.0)*1600.0; // from degree/s to step/s
+        speed1 = ((filtered1/360.0)*(2*PI))*wheel_radius; // convert to rad/s then cm/s
+        speed1s = (filtered1/360.0)*1600;
       } 
+
+      stepper0.runSpeed();
+      stepper1.runSpeed();
+      stepper2.runSpeed();
 
       ///////////// wheel 2 ///////////////
       tcaselect(2);
@@ -286,21 +620,60 @@ void loop()
       if (filter_flag == 1)
       {
         // kalman filter
-        filtered2 = KF_filter(2, gyroReading2);
+        filtered2 = KF_filter(2, gyroReading2); // degree/s
     
         // converting unit
-        speed2 = (filtered2/360.0)*1600.0; // from degree/s to step/s
+        speed2 = ((filtered2/360.0)*(2*PI))*wheel_radius; // convert to rad/s then cm/s
+        speed2s = (filtered2/360.0)*1600;
       } 
+
+      stepper0.runSpeed();
+      stepper1.runSpeed();
+      stepper2.runSpeed();
           
       filter_flag = 1; // After get the first data, this flag set to 1. 
                        // Having this flag helps me to know which data is the previous data and which is the current, just aquired data.
 
       ///////// Done getting and filtering data ///////////
+      ///////// Start integration to find (x,y) ///////////
+
+      // recalculate v_x, v_y, w
+	  w = 1/(3*rover_radius)*(speed0 + speed1 + speed2);
+	  v = (sqrt(3)/3)*(speed2 - speed0);
+	  v_n = 1/3.0*(speed2 + speed0) - 2/3.0*speed1;
+
+	  v_x = v*cos(phi) - v_n*sin(phi);
+	  v_y = v*sin(phi) + v_n*cos(phi);
+
+	  elapsedTime2 = millis() - start2;
+	  start2 = millis();
+
+	  // calculate x, y phi
+	  x = x + v_x*elapsedTime2/1000.0;
+	  y = y + v_y*elapsedTime2/1000.0;
+	  phi = phi + w*elapsedTime2/1000.0;
+
+  	stepper0.setSpeed(v0s);
+	  stepper1.setSpeed(v1s);
+	  stepper2.setSpeed(v2s);
+
+	  // stopping condition
+	  if (abs(dx) < 0.5 && abs(dy) < 0.5 && abs(dphi) < 0.015)
+	  {
+	    // stop
+	    stepper0.stop();
+	    stepper1.stop();
+	    stepper2.stop();
+
+	    stepper0.setSpeed(0);
+	    stepper1.setSpeed(0);
+	    stepper2.setSpeed(0);
+
+      start_flag = 0;
+	  } 
    }
 
-    stepper0.runSpeed();
-    stepper1.runSpeed();
-    stepper2.runSpeed();
-
-   
+   stepper0.runSpeed();
+   stepper1.runSpeed();
+   stepper2.runSpeed();
 }
