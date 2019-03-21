27f122aee314f1fcab6802080acdc64af100775c
vitsensei <42131264+vitsensei@users.noreply.github.com>
2019-03-21 21:48:43 +1030

    Update and rename rapi.py to hostComp_controller.py

---
+++ controller/hostComp_controller.py
@@ -0,0 +1,54 @@
+import socket
+
+def coor2bytes(coor_fnc):
+	coorByte = [0, 0, 0, 0, 0, 0]
+
+	if (coor_fnc[0] >= 0):
+		coorByte[0] = (coor_fnc[0] >> 8) & 0xFF # High byte of X
+		coorByte[1] = coor_fnc[0] & 0xFF # Low byte of X
+	else:
+		coor_fnc[0] = coor_fnc[0]*(-1)
+		coorByte[0] = (coor_fnc[0] >> 8) & 0xFF # High byte of X
+		coorByte[0] = coorByte[0] ^ 0x80
+		coorByte[1] = coor_fnc[0] & 0xFF # Low byte of X
+	
+	if (coor_fnc[1] >= 0):
+		coorByte[2] = (coor_fnc[1] >> 8) & 0xFF # High byte of Y
+		coorByte[3] = coor_fnc[1] & 0xFF # Low byte of Y
+	else:
+		coor_fnc[1] = coor_fnc[1]*(-1)
+		coorByte[2] = (coor_fnc[1] >> 8) & 0xFF # High byte of X
+		coorByte[2] = coorByte[2] ^ 0x80
+		coorByte[3] = coor_fnc[1] & 0xFF # Low byte of X
+		
+	if (coor_fnc[2] >= 0):
+		coorByte[4] = (coor_fnc[2] >> 8) & 0xFF # High byte of Phi
+		coorByte[5] = coor_fnc[2] & 0xFF # Low byte of Phi
+	else:
+		coor_fnc[2] = coor_fnc[2]*(-1)
+		coorByte[4] = (coor_fnc[2] >> 8) & 0xFF # High byte of Phi
+		coorByte[4] = coorByte[4] ^ 0x80
+		coorByte[5] = coor_fnc[2] & 0xFF # Low byte of Phi
+
+	return coorByte
+
+if __name__ == '__main__':
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+	s.bind((socket.gethostname(), 1234)) # bind(ip, port)
+	print("Done binding.")
+	s.listen(5)
+
+	clientsocket, address = s.accept()
+	print(f"Connection from {address} has been established!")
+
+	while True:
+		coor = [0, 0, 0]
+
+		coor[0] = int(input("X: "))
+		coor[1] = int(input("y: "))
+		coor[2] = int(input("phi: "))
+
+		coorByte = coor2bytes(coor)
+
+		clientsocket.send(bytearray(coorByte))
