2a8a18f0d42de538e9d09462f67035394e49e735
vitsensei <42131264+vitsensei@users.noreply.github.com>
2019-04-13 16:42:34 +0930

    Update mkr1000.ino
    Add new thing to support new update to the Due.

--- controller/mkr1000.ino
+++ controller/mkr1000.ino
@@ -11,72 +11,110 @@ WiFiClient client;
 
 
 // Random variable
-int a, i, j, k;
-int trash_count = 0;
-byte trash;
+int a, i, j, k, m;
 byte buf0[7];
 byte buf1[7];
+byte buf2[7];
+
+long start = millis();
+int elapsedTime = 0;
+int timeout = 0;
 
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(115200);
   Serial1.begin(115200);
 
-  pinMode(LED_BUILTIN, OUTPUT);
   
-  status = WiFi.begin(ssid, pass);
-  if (status != WL_CONNECTED) {
-    while (status != WL_CONNECTED) {
-      status = WiFi.begin(ssid, pass);
-    }
-  } else  {
-    j = client.connect(server, 1234);
-    while (j != 1)  {
-      j = client.connect(server, 1234);
-    }
+//  status = WiFi.begin(ssid, pass);
+
+  while (status != WL_CONNECTED) {
+    status = WiFi.begin(ssid, pass);
   }
+
+  delay(10000);
+  
+  j = client.connect(server, 1234);
+  while (j != 1)  {
+    j = client.connect(server, 1234);
+  }
+
 }
 
 void loop()
 {
-	if (client.available()) {
-//    Serial.println("I got something.");
+	if (client.available()) {   
 		a = client.read();
-
 		if (a == 0)	{ // Send new target to Due
       j = 0;
+      start = millis();
 			while(j<6)	{
+        elapsedTime = millis() - start;
+
+        if (elapsedTime > 1000) {
+          timeout = 1;
+          break;
+        }
+        
 				if (client.available()>0)	{
 					buf0[j] = client.read();
-					j++;
+					j++;        
 				}
 			}
 
+     if (timeout != 1)  {
       Serial1.write((byte) 0);
       // Send coordinate back to Due
       for (i = 0; i<6; i++) {
         Serial1.write(buf0[i]);
       }
+     } else  {
+      timeout = 0;
+     }
 
 		} else if (a == 1)	{
-//      Serial.println("Hi.");
       // Get the coordinate from the Due
       Serial1.write((byte) 1);
       k = 0;
+      start = millis();
       while(k < 6) {
+        elapsedTime = millis() - start; 
+
+        if (elapsedTime > 1000) {
+          timeout = 1;
+          break;
+        }
+               
         if (Serial1.available() > 0)  {
           buf1[k] = Serial1.read();
-          k++;
+          k++;          
         }      
       }
 
-			for (i=0;i<6;i++)	{
-				client.write(buf1[i]);
-			}
+      if (timeout != 1) {
+        client.write(buf1,6);
+        client.write((byte) 0);  // fill in the blank size
+        client.write((byte) 0);
+      } else {
+        timeout = 0;
+      }
 
-      client.write((byte) 0);  // fill in the blank size
-      client.write((byte) 0);
-		} else {
-		}
+		} else if (a == 2) { // set the current coordinnate to be something else.
+      m = 0;
+      while(m<6)  {
+        if (client.available()>0) {
+          buf2[m] = client.read();
+          m++;
+        }
+      }
+
+      Serial1.write((byte) 2);
+      // Send coordinate back to Due
+      for (i = 0; i<6; i++) {
+        Serial1.write(buf2[i]);
+      }
+
+    } else if (a == 3) { // identify yourself
+      client.write((byte) 1);
+    }
 	}
 }
